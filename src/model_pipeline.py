56cb08238619aff80d95a44eb5a29c3a12c44fc6
mariacsalazar <mcsalazart@gmail.com>
2024-10-21 23:03:05 +0200

    Model pipeline changed

--- src/model_pipeline.py
+++ src/model_pipeline.py
@@ -7,10 +7,11 @@ import torch.optim as optim
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-import os
+from datetime import datetime
+from pathlib import Path
 
 
-def compute_accuracy(preds, y):
+def get_accuracy(preds, y):
     """
     Compute the accuracy
 
@@ -68,7 +69,7 @@ def initialize_model(num_classes, learning_rate):
     print("initiated model")
     return model, optimizer, loss_fn, device
     
-def train_one_epoch(model, train_loader, optimizer, loss_fn, device):
+def train_one_epoch(epoch,model, train_loader, optimizer, loss_fn, device):
     
     """
     Train the model for one epoch. It iterates over the
@@ -92,12 +93,12 @@ def train_one_epoch(model, train_loader, optimizer, loss_fn, device):
         optimizer.step()
 
         # Compute accuracy
-        accuracy = compute_accuracy(train_preds, y)
+        accuracy = get_accuracy(train_preds, y)
         epoch_train_loss += loss.item()
         epoch_train_acc += accuracy
 
         # Print results per batch
-        print(f"Batch [{batch_idx + 1}/{len(train_loader)}] - Loss: {loss.item():.4f}, Accuracy: {accuracy:.4f}")
+        print(f"Epoch {epoch},Batch [{batch_idx + 1}/{len(train_loader)}] - Loss: {loss.item():.4f}, Accuracy: {accuracy:.4f}")
 
     avg_train_loss = epoch_train_loss / len(train_loader)
     avg_train_accuracy = epoch_train_acc / len(train_loader)
@@ -124,7 +125,7 @@ def evaluate_model(model, test_loader, loss_fn, device):
             test_preds = model(X_test)
             loss_test = loss_fn(test_preds, y_test)
 
-            accuracy_test = compute_accuracy(test_preds, y_test)
+            accuracy_test = get_accuracy(test_preds, y_test)
 
             epoch_test_loss += loss_test.item() * X_test.size(0)
             epoch_test_acc += accuracy_test * X_test.size(0)
@@ -149,12 +150,23 @@ def train_and_save_model(num_epochs, batch_size, learning_rate, num_workers, che
         print(f"\n--- Epoch {num_epoch}/{num_epochs} ---")
         
     
-        avg_train_loss, avg_train_accuracy = train_one_epoch(model, train_loader, optimizer, loss_fn, device)
+        avg_train_loss, avg_train_accuracy = train_one_epoch(num_epoch,model, train_loader, optimizer, loss_fn, device)
         train_loss.append(avg_train_loss)
         train_accuracy.append(avg_train_accuracy)
 
-    
+        today = datetime.now()
+        datestring = today.strftime("checkpoints_%Y_%m_%d__%H")  
+
+
+
+        Path(datestring).mkdir(parents=True, exist_ok=True)
+        
+        checkpoints_dir = os.path.join(datestring, "checkpoints")
+        os.mkdir(checkpoints_dir)
+
+           
         if num_epoch % checkpoint_interval == 0:
+            checkpoint_path = os.path.join(checkpoints_dir, f'model_checkpoint_epoch_{num_epoch}.pt')
             torch.save(model.state_dict(), f'model_checkpoint_epoch_{num_epoch}.pt')
             print(f'Checkpoint saved: model_epoch_{num_epoch}.pt')
 
@@ -165,21 +177,24 @@ def train_and_save_model(num_epochs, batch_size, learning_rate, num_workers, che
             test_accuracy.append(avg_test_accuracy)
             print(f'Test Loss: {avg_test_loss:.4f}, Test Accuracy: {avg_test_accuracy:.4f}')
 
-    
-    torch.save(model.state_dict(), 'final_model.pt')
+    checkpoint_path = os.path.join(checkpoints_dir, f'final_model.pt')
+    torch.save(model.state_dict(),checkpoint_path)
     print('Final model saved in "final_model.pt"')
 
     return train_loss, train_accuracy, test_loss, test_accuracy
 
+if __name__ == "__main__":
+       abspath = os.path.abspath(__file__)
+       dname = os.path.dirname(abspath)
+       os.chdir(dname)
 
-train_loss, train_accuracy, test_loss, test_accuracy = train_and_save_model(
-    num_epochs=1, 
-    batch_size=64, 
-    learning_rate=0.001, 
-    num_workers=4, 
-    checkpoint_interval=5, 
-    test_interval=2, 
-    train_folder='../data/train', 
-    test_folder='../data/test'
-)
+       train_loss, train_accuracy, test_loss, test_accuracy = train_and_save_model(
+        num_epochs=1, 
+        batch_size=64, 
+        learning_rate=0.001, 
+        num_workers=4, 
+        checkpoint_interval=5, 
+        test_interval=2, 
+        train_folder='../data/train', 
+        test_folder='../data/test')
 
